c31222a8bf115ef8b85424437f34f8574372f79c
Salas <94945634+SalasJ13@users.noreply.github.com>
2022-01-10 11:56:29 -0800

    Add files via upload

--- scripts/register.js
+++ scripts/register.js
@@ -1,1 +1,67 @@
+let saloon={
+    name:"The Fashion Pet",
+    address:{
+        street:"Av.Lion",
+        Number:"34",
+        zip:"2210",
+        city:"San Diego",
+        state:"California"
+    },
+    hours:{
+        open:"9:00am",
+        close:"5:00pm"
+    },
+    pets:[
+        {
+            name:"Chucky",
+            age:10,
+            gender:"Male",
+            breed:"Doberman",
+            service:"Consult",
+            ownerName:"Jazmin",
+            contactPhone:"665-234-3532"
+        },
+        {
+            name:"Querubin",
+            age:10,
+            gender:"Female",
+            breed:"Pastor Aleman",
+            service:"Consult",
+            ownerName:"Jojo",
+            contactPhone:"665-344-3532"
+        },
+        {
+            name:"Jojo",
+            age:1,
+            gender:"Male",
+            breed:"Chihuahua",
+            service:"Consult",
+            ownerName:"Omar",
+            contactPhone:"665-344-3532"
+        }
 
+    ]
+}
+//display the information of the pat saloon address on the footer
+function displayInfo(){
+    document.getElementById("footer-info").innerHTML=`
+    <p>${saloon.address.street},
+    ${saloon.address.Number}, ZIP
+    Code: ${saloon.address.zip}</p>
+    <p>${saloon.address.city}
+    ${saloon.address.state},
+    </p>`
+}
+
+function displayPetNames(){
+    console.log(saloon.pets[0].name);
+    console.log(saloon.pets[1].name);
+    console.log(saloon.pets[2].name);
+}
+
+function viewAlert(){
+    
+    alert("Pets are registered: " + saloon.pets.length);
+}
+displayInfo();
+viewAlert();
